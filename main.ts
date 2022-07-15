b9fba134020f2a19829b875250f780b9db130b66
Evan Wagner <36539810+EvanWagner2@users.noreply.github.com>
2022-07-15 10:35:32 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+// variables 
+let player: game.LedSprite = game.createSprite(2,4);
+let asteroid: game.LedSprite = null;
+let xAcceleration = 0;
+let delay = 1000;
+let laser: game.LedSprite = null;
+
+// asteroid movement 
+basic.forever(function(){
+    asteroid = game.createSprite(Math.randomRange(4,0), 0);
+
+// move asteroid 
+while(asteroid.get(LedSpriteProperty.Y)<4){
+    asteroid.change(LedSpriteProperty.Y, 1);
+    basic.pause(delay)
+}
+// speed up asteroids
+delay = delay - delay /10;
+})
+
+// player controls
+basic.forever(function(){
+    xAcceleration = input.acceleration(Dimension.X)
+
+    // move player
+    if(xAcceleration < -150 && player.get(LedSpriteProperty.X)>0){
+        player.change(LedSpriteProperty.X, -1)
+    } else if (xAcceleration > 150 && player.get(LedSpriteProperty.X)<4){
+        player.change(LedSpriteProperty.X, 1)
+    }
+    basic.pause(50);
+})
+
+// firing lasers
+basic.forever(function(){
+input.onButtonPressed(Button.AB, function() {
+    laser = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
+    // move lasers
+    while(laser.get(LedSpriteProperty.Y)>= 1){
+        laser.change(LedSpriteProperty.Y, -1);
+        basic.pause(50);
+    }
+    laser.delete();
+})
+})
